cb1a6f3bdbd5fb51429b04054b42fe0db6470235
Chrystian Rocha <chrystianrbueno@gmail.com>
2021-10-19 12:47:07 -0300

    Micronaut -> Spring - Recebendo objeto com string

--- order-svc-kafka-spring/src/main/java/com/example/kafka/ConsumerKafkaConfig.java
+++ order-svc-kafka-spring/src/main/java/com/example/kafka/ConsumerKafkaConfig.java
@@ -1,19 +1,13 @@
 package com.example.kafka;
 
-import com.example.domain.Shipment;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
--- order-svc-kafka-spring/src/main/java/com/example/kafka/ShipmentConsumer.java
+++ order-svc-kafka-spring/src/main/java/com/example/kafka/ShipmentConsumer.java
@@ -3,11 +3,7 @@ package com.example.kafka;
 import com.example.domain.Shipment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
